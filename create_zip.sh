83853095df1c27c1f7b7c877cf03936415bb4689
Joy Yeh <c-yeh@mail.nissan.co.jp>
2024-02-15 15:07:31 +0800

    Separate target python versions

--- create_zip.sh
+++ create_zip.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-platform=${1:-win_amd64}
-tmp_dir=packed-awscli_$platform
+platform=${platform:-win_amd64}
+pyver=${pyver:-3.10}
+tmp_dir=packed-awscli_${platform}_py${pyver}
 
-pip download -d $tmp_dir awscli --platform $platform --only-binary=:all:
+pip download -d $tmp_dir awscli \
+    --platform $platform --only-binary=:all: \
+    --python-version $pyver
 zip -r $tmp_dir.zip $tmp_dir
 
 rm -rf $tmp_dir
