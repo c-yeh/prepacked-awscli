ec4694f918206807d90b6d2546301dead00d4517
Joy Yeh <c-yeh@mail.nissan.co.jp>
2024-02-15 16:31:01 +0800

    Pump to awscli v2

--- create_zip.sh
+++ create_zip.sh
@@ -4,9 +4,14 @@ platform=${platform:-win_amd64}
 pyver=${pyver:-3.10}
 tmp_dir=packed-awscli_${platform}_py${pyver}
 
-pip download -d $tmp_dir awscli \
-    --platform $platform --only-binary=:all: \
-    --python-version $pyver
-zip -r $tmp_dir.zip $tmp_dir
-
-rm -rf $tmp_dir
+platform_arg="--platform $platform"
+if [ $platform == 'this' ]; then
+    platform_arg=''
+fi
+python3 -m pip download -d $tmp_dir \
+    git+https://github.com/aws/aws-cli.git@v2 \
+    flit_core \
+    $platform_arg --only-binary=:all: \
+    --python-version $pyver \
+&& zip -r $tmp_dir.zip $tmp_dir \
+&& rm -rf $tmp_dir
